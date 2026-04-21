f8ed45f6535fced291b3ad8c342c07070b737035
Zeegeedee <128145085+Zeegeedee@users.noreply.github.com>
2026-04-21 19:40:17 +0800

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG:
+
+## V1.1:
+- Added CHANGELOG.md
+- Organized the files further
+- Fixed a bug that made the dictionary attack vulnerability detector less strict than it should be
+- Added more checkers if your password is part of famous mathematical constants
---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import time
+from src.app import run_checker
+
+VERSION = "v1.1"
+
+if __name__ == "__main__":
+    if "main.py" in sys.argv[0]:
+        print(f"Version: {VERSION}")
+        run_checker()
+    else:
+        print("Error: The file name is wrong.")
+        print("Have you changed it to anything that's not 'main.py'?")
+        time.sleep(3)
+        sys.exit()
